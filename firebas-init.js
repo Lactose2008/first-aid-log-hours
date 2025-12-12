c57da577452938b705ea5d57121b082c3d3c34c5
Lactose2008 <lactoseofyourlife31@gmail.com>
2025-12-12 06:55:29 +0200

    Comment out Firebase imports in firebas-init.js

--- firebas-init.js
+++ firebas-init.js
@@ -1,6 +1,6 @@
 // firebase-init.js
 // Shared Firebase initialization (module)
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+/*import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-storage.js";
